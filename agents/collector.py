d18a0875ecadd09bed03b7bd5e82175e5e83d518
Rachit Srivastava <124511652+rs-exp@users.noreply.github.com>
2026-07-15 01:30:22 +0530

    feat: add ArbeitNow scraper and multi-source collection

--- agents/collector.py
+++ agents/collector.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from time import perf_counter
 
 from database.job_repository import JobRepository
+from scrapers.arbeitnow_scraper import ArbeitNowScraper
 from scrapers.base_scraper import BaseScraper
 from scrapers.remotive_scraper import RemotiveScraper
 from utils.logger import get_logger
@@ -31,6 +32,7 @@ class JobCollector:
 
         self.scrapers: list[BaseScraper] = [
             RemotiveScraper(),
+            ArbeitNowScraper(),
         ]
 
     def run(self) -> CollectionSummary:
